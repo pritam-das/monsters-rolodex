a42f31aabd646d82ad2719463670ec8732440bda
Pritam Das <pritam.das@brisksynergies.com>
2020-08-18 09:48:41 -0400

    Updates

---
+++ precache-manifest.9da96535c04caa250ad923bb325ffc52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a1f9152a43da0412e30d1b551e7131",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c2f483b7733be8a09f8",
+    "url": "/monsters-rolodex/static/css/main.c7b99c43.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2f483b7733be8a09f8",
+    "url": "/monsters-rolodex/static/js/main.45e0d0ca.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
